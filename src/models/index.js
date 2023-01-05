e1d93f68c1f98eab50ff5af9ba3703962de36a62
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2023-01-04 20:23:38 -0800

    lab 8 - access control

---
+++ src/models/index.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require('sequelize');
+
+const { makeAuthUser } = require('../auth/models/users-model');
+
+const { makeBlog } = require('./blog.model');
+
+const DATABASE_URL =
+  process.env.NODE_ENV === 'test'
+    ? 'sqlite::memory:'
+    : process.env.DATABASE_URL;
+
+const CONNECTION_OPTIONS =
+  process.env.NODE_ENV === 'test'
+    ? {
+        // logging: false,
+      }
+    : {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      };
+
+const sequelize = new Sequelize(DATABASE_URL, CONNECTION_OPTIONS);
+
+const AuthUser = makeAuthUser(sequelize);
+
+const Blog = makeBlog(sequelize);
+
+module.exports = {
+  sequelize,
+  AuthUser,
+  Blog,
+};
